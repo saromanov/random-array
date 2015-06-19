e02dbf79581ed700ff979535a7d92e4fc852de57
Sergey Romanov <xxsmotur@gmail.com>
2015-06-19 21:49:38 +0500

    add genWhile

--- index.js
+++ index.js
@@ -20,6 +20,22 @@ module.exports = function(minvalue, maxvalue){
     twod: function(width,height, options){
             return Array.apply(null, {length: width}).map(function(){ return genRandomArray(height, minvalue, maxvalue, options);}); 
     },
+
+    genWhile: function(func, options) {
+        var result = [];
+        var opt = options || {};
+        var limit = opt.limit || 10000;
+        var c = 0;
+        while(c !== limit){
+            var item = Math.random() * (maxvalue - minvalue) + minvalue;
+            result.push(item);
+            if(func(item)) {
+                return result;
+            }
+            c += 1;
+        }
+        return result;
+    },
    };
 };
 
